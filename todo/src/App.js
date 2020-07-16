cc3a368c819e566fe6d223c480f0a9fe3d6eadbb
shiryaevam <shiryaev.am@mail.ru>
2020-07-16 22:36:43 +0400

    feat add TodoItem and List

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,9 +1,28 @@
 import React from "react";
+import TodoList from "./Todo/Todolist";
 
 function App() {
+  const [todos, setTodos] = React.useState([
+    { id: 1, completed: false, title: "Купить квас" },
+    { id: 2, completed: false, title: "Купить пиво" },
+    { id: 3, completed: false, title: "Купить сыр" },
+  ]);
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    );
+  }
+
   return (
     <div className="wrapper">
       <h1>Todo</h1>
+      <TodoList todos={todos} onToggle={toggleTodo} />
     </div>
   );
 }
