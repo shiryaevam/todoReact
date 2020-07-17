a6fc5021f4bc82522ca6063deab73cff460ee75e
shiryaevam <shiryaev.am@mail.ru>
2020-07-17 14:16:00 +0400

    feat: add context

--- todo/src/Todo/TodoItem.js
+++ todo/src/Todo/TodoItem.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
+import Context from "../context";
 
 const styles = {
   li: {
@@ -17,6 +18,7 @@ const styles = {
 };
 
 function TodoItem({ todo, index, onChange }) {
+  const { removeTodo } = useContext(Context);
   const classes = [];
 
   if (todo.completed) {
@@ -36,7 +38,9 @@ function TodoItem({ todo, index, onChange }) {
         &nbsp;
         {todo.title}
       </span>
-      <button className="rm">&times;</button>
+      <button className="rm" onClick={removeTodo.bind(null, todo.id)}>
+        &times;
+      </button>
     </li>
   );
 }
